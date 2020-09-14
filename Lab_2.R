e8c7de9ef8425ef3835b32ca9b8a03e1fcf1ddd3
hdenton23 <70909815+hdenton23@users.noreply.github.com>
2020-09-14 13:34:15 -0400

    Uploaded Lab 2.

---
+++ Lab_2.R
@@ -0,0 +1,34 @@
+#1
+library(tidyverse)
+install.packages("datasauRus")
+library(datasauRus)
+
+dino_data<- datasaurus_dozen %>%
+filter(dataset=="dino")  
+
+#2
+ggplot(data=dino_data)+
+  geom_point(mapping=aes(x=x, y=y))
+
+circle_data<- datasaurus_dozen %>%
+  filter(dataset=="circle")
+ggplot(data=circle_data)+
+  geom_point(mapping=aes(x=x, y=y))
+
+star_data<- datasaurus_dozen %>%
+  filter(dataset=="star")
+ggplot(data=star_data)+
+  geom_point(mapping=aes(x=x, y=y))
+
+#3
+dino_data %>%
+  summarize(r = cor(x,y))
+
+circle_data %>%
+  summarize(r = cor(x,y))
+
+star_data %>%
+  summarize(r = cor(x,y))
+
+#4
+
